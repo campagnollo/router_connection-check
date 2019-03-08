6ff3f2cba65a31174062e3a30a53fe8e4691fdc5
Eric <campagnollo2002@gmail.com>
2019-03-08 10:55:12 -0500

    Add files via upload

--- rtrcheck.py
+++ rtrcheck.py
@@ -7,23 +7,36 @@ def show(remote, command):
 
     stdin, stdout, stderr=remote.exec_command(command)
     result = stdout.read()
-    i=240
-    while result[i]!="U" and result[i]!="N":
+    i=250
+    while result[i]!="U" and result[i]!="N" and result[i]!="P" and result [i]!="p":
        i+=1
     print(result[i:]+"\n") #print the output sans banner
 
-
-
-
 def main(router):
-    user="***"
-    password="***"
+    d={}
+    with open("sites.txt") as site:
+        for line in site:
+          key, val = line.split()
+          d[str(key)] = val
+    if router[0]=="P" or router[0]=="p":
+        user=""
+        password="****"
+    else:
+        user="****"
+        password="****"
     commands=("sh ver | i uptime","sh bgp ipv4 uni sum | b Neighbor","sh bgp ipv6 uni sum | b Neighbor")
     try:
        ip=socket.gethostbyname(router[1]) #ID device name or pass IP address
     except socket.gaierror:
        print("Device not identified on DNS")
        exit()
+    try:
+        if d[ip]=="True":
+           for i in range (0,5):
+               print("*****critical******")
+    except KeyError:
+       print("Device not found on critical db")
+	   
     print("\r") #White space
     for i in range (0,3):
        remote = paramiko.SSHClient()
@@ -33,6 +46,7 @@ def main(router):
           remote_transport=remote.get_transport()
        except paramiko.AuthenticationException:
           print("Authentication failed")
+          exit()
        else:
           show(remote,commands[i]) #If connected, run through 'show' commands
        remote.close()
