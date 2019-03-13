cd576bb39fb98837c8524965efcd1dd986bd27e9
Eric <campagnollo2002@gmail.com>
2019-03-13 16:10:06 -0400

    Add files via upload

--- rtrcheck.py
+++ rtrcheck.py
@@ -3,44 +3,73 @@ import paramiko
 import socket
 from time import sleep
 
-def show(remote, command):
+def show(remote, command, EPA):
 
     stdin, stdout, stderr=remote.exec_command(command)
     result = stdout.read()
     i=250
-    while result[i]!="U" and result[i]!="N" and result[i]!="P" and result [i]!="p":
-       i+=1
-    print(result[i:]+"\n") #print the output sans banner
+    if not(EPA):
+        while result[i]!="U" and result[i]!="N" and result[i]!="P" and result [i]!="p":
+           i+=1
+        print(result[i:]+"\n") #print the output sans banner
+    else:
+        print(result)
 
 def main(router):
+    ATTuser="****"
+    ATTpassword="****"
+    TACACSuser="****"
+    TACACSpassword="****"
+	commands=("sh bgp ipv4 uni sum | b Neighbor","sh bgp ipv6 uni sum | b Neighbor")
     d={}
+
     with open("sites.txt") as site:
         for line in site:
-          key, val = line.split()
-          d[str(key)] = val
-    if router[0]=="P" or router[0]=="p":
-        user=""
-        password="****"
-    else:
-        user="****"
-        password="****"
-    commands=("sh ver | i uptime","sh bgp ipv4 uni sum | b Neighbor","sh bgp ipv6 uni sum | b Neighbor")
+			sitedb=line.split()
+			d[str(sitedb[0])] = [sitedb[1],sitedb[2]] #Load strings into dictionary
     try:
-       ip=socket.gethostbyname(router[1]) #ID device name or pass IP address
-    except socket.gaierror:
+		ip=socket.gethostbyname(router[1]) #ID device name or pass IP address
+		if d[ip][0]=="True":
+			for i in range (0,5):
+				print("*****critical******")
+			   
+	except socket.gaierror: #Exception if IP isn't found
        print("Device not identified on DNS")
-       exit()
-    try:
-        if d[ip]=="True":
-           for i in range (0,5):
-               print("*****critical******")
-    except KeyError:
+       exit()		   
+    except KeyError: #Exception if IP not found in dictionary "d"
        print("Device not found on critical db")
+       exit()
 	   
+    if d[ip][1]=="EPA": #Use these credentials if with EPA
+        user=TACACSuser
+        password=TACACSpassword
+        EPA=True
+		
+    else:  #Use credentials if with AT&T
+        user=ATTuser
+        password=ATTpassword
+        EPA=False
+		
     print("\r") #White space
-    for i in range (0,3):
-       remote = paramiko.SSHClient()
-       remote.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        remote=paramiko.SSHClient()
+        remote.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        remote.connect(hostname=ip, port=22,username=user,password=password, timeout=10)
+        remote_transport=remote.get_transport()
+
+    except paramiko.AuthenticationException:
+        print("Authentication failed")
+        exit()
+
+    else:
+        stdin, stdout, stderr=remote.exec_command("sh ver | i uptime")
+        print(stdout.read())
+
+    finally:
+        remote.close()
+
+
+    for i in range (0,len(commands)):
        try:
           remote.connect(hostname=ip, port=22,username=user,password=password, timeout=10)
           remote_transport=remote.get_transport()
@@ -48,14 +77,15 @@ def main(router):
           print("Authentication failed")
           exit()
        else:
-          show(remote,commands[i]) #If connected, run through 'show' commands
+          show(remote,commands[i],EPA) #If connected, run through 'show' commands
        remote.close()
 
 if __name__ == '__main__':
-#"""Python interpreter check"""
+"""Python interpreter check"""
     try:
         assert sys.version_info[0]<3
     except AssertionError:
         print("Incorrect interpreter being run. Please use Python 2.x")
         exit()
     main(sys.argv)
+
